197f9cd250b9e744c658e00d37083bfde8ba7f84
Willis Blackburn <wboyce@panix.com>
2011-12-29 13:02:18 -0500

    Added some documentation to ScopeVariablesPage.

--- examples/features/src/main/scala/org/errandframework/examples/features/ScopeVariablesPage.scala
+++ examples/features/src/main/scala/org/errandframework/examples/features/ScopeVariablesPage.scala
@@ -1,9 +1,9 @@
 package org.errandframework.examples.features
 
 import org.errandframework.components.ActionLink
-import org.errandframework.http.{RedirectResponse, ApplicationVar, SessionVar, RequestVar}
 
 import FeaturesExampleApplication._
+import org.errandframework.http._
 
 /**
  * Demonstrates the various scope variables:  RequestVar, SessionVar, and ApplicationVar.
@@ -14,15 +14,28 @@ class ScopeVariablesPage extends FeaturePage {
 
   def featureContent = {
     <div>
+      <p>No matter how many times you click "Increment," the RequestVar will always be zero, because it is reset
+        to zero every time the page is loaded.</p>
       <p>RequestVar value: {r.value} {rincLink.render}</p>
+      <p>The SessionVar will increase by one every time you click "Increment," but if you {clearSessionLink.render} or
+        view the page from another browser, you will see that it resets back to zero.</p>
       <p>SessionVar value: {s.value} {sincLink.render}</p>
+      <p>The ApplicationVar will increase by one every time you click "Increment," and the change will be visible
+        to all other users.</p>
       <p>ApplicationVar value: {a.value} {aincLink.render}</p>
     </div>
   }
 
-  private val rincLink = ActionLink("Increment") { r.value += 1; RedirectResponse(scopeVariablesLocation.toUrl()) }
-  private val sincLink = ActionLink("Increment") { s.value += 1; RedirectResponse(scopeVariablesLocation.toUrl()) }
-  private val aincLink = ActionLink("Increment") { a.value += 1; RedirectResponse(scopeVariablesLocation.toUrl()) }
+  private val rincLink = ActionLink("Increment") { r.value += 1; redirectHere }
+  private val sincLink = ActionLink("Increment") { s.value += 1; redirectHere }
+  private val aincLink = ActionLink("Increment") { a.value += 1; redirectHere }
+
+  private val clearSessionLink = ActionLink("clear the session") {
+    RequestContext.httpSession.invalidate()
+    redirectHere
+  }
+
+  private val redirectHere = RedirectResponse(scopeVariablesLocation.toUrl())
 
   override def title = "Scope Variables"
 }
